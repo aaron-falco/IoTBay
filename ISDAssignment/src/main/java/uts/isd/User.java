70e776a1216417d6ab77ba793f4d052ed8cb9f0d
tek-f <aaron.a.falco@student.uts.edu.au>
2025-03-28 14:21:20 +1100

    Added Assignment 1 Required Classes
    Created JSP files:
    - login
    - logout
    - welcome
    - landing
    - register
    - main
    
    Created User.java

---
+++ ISDAssignment/src/main/java/uts/isd/User.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package uts.isd;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author aaron
+ */
+public class User implements Serializable{
+    private String username;
+    private String password;
+    private String email;
+    private String dob;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }    
+}
